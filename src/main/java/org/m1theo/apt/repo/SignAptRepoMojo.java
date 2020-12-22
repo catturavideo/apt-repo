7938e795f7b94482cae503da3ac13875168c8daf
Gregory Marco <greg@catturavideo.com>
2020-12-22 23:17:44 +0000

    remove unused imports

--- src/main/java/org/m1theo/apt/repo/SignAptRepoMojo.java
+++ src/main/java/org/m1theo/apt/repo/SignAptRepoMojo.java
@@ -10,12 +10,9 @@ package org.m1theo.apt.repo;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
 import org.bouncycastle.openpgp.PGPException;
 import org.codehaus.plexus.util.FileUtils;
